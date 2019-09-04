a86828ba120ef07c40564b62b930ce22c3c16fcb
Markus Rothlübbers <markus@mrlb.space>
2019-09-04 22:27:50 +0200

    unchanged german files added

---
+++ resources/lang/de/seeder.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Seeders
+    |--------------------------------------------------------------------------
+    |
+    | Text used in database seeders.
+    |
+    */
+
+    // Venues
+    'example-venue' => 'Beispiel Veranstaltung',
+    'example-venue-description' => 'Beispiel Beschreibung',
+
+    // LANs
+    'example-lan' => 'Beispiel LAN',
+
+    // Roles
+    'super-admin' => 'Super Admin',
+    'admin' => 'Admin',
+
+    // Steam User Status Codes
+    'offline' => 'Offline',
+    'online' => 'Online',
+    'busy' => 'Beschäftigt',
+    'away' => 'Abwesend',
+    'snooze' => 'Schlummern',
+    'looking-to-trade' => 'Will tauschen',
+    'looking-to-play' => 'Will spielen',
+
+    // Slides
+    'slide-lanager' => '# Melde dich bei :app-url an um an der Party teilzunehmen!',
+    'slide-events' => ':app-url/events/fullscreen',
+    'slide-games' => ':app-url/games/fullscreen',
+];
---
+++ resources/lang/de/title.php
@@ -0,0 +1,163 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Links
+    |--------------------------------------------------------------------------
+    |
+    | Usage examples:
+    | - Links
+    | - Buttons
+    |
+    | Text should:
+    | - Use title case
+    | - Not be terminated with a full stop (.)
+    | - Only be a few words long
+    */
+
+    // Multi-purpose
+    'title' => 'Titel',
+    'submit' => 'Abschicken',
+    'content' => 'Inhalt',
+    'published' => 'Veröffentlicht',
+    'unpublished' => 'Unveröffentlicht',
+    'admin' => 'Admin',
+    'start' => 'Start',
+    'end' => 'Ende',
+    'item-name' => ':Item Name',
+    'name' => 'Name',
+    'unknown' => 'Unbekannt',
+    'actions' => 'Aktionen',
+    'api' => 'API',
+    'colour' =>  'Farbe',
+    'status' => 'Status',
+    'type' => 'Typ',
+    'description' => 'Beschreibung',
+    'copy-markdown-link' => 'Kopiere Markdown Link',
+    'fullscreen' => 'Vollbild',
+
+    // CRUD
+    'create-item' => 'Erstelle :Item',
+    'edit-item' => 'Bearbeite :Item',
+    'update-item' => 'Aktualisiere :Item',
+    'delete-item' => 'Lösche :Item',
+    'create' => 'Erstellen',
+    'edit' => 'Bearbeiten',
+    'update' => 'Aktualisieren',
+    'delete' => 'Löschen',
+    'manage' => 'Verwalten',
+    'created' => 'Erstellt',
+    'updated' => 'Aktualisiert',
+
+    // Authentication
+    'sits' => 'Anmelden über Steam',
+    'login' => 'Anmelden',
+    'logout' => 'Abmelden',
+
+    // Users
+    'user' => 'User',
+    'username' => 'Username',
+    'full-name' => 'Full Name',
+    'users' => 'Users',
+    'profile' => 'Profile',
+    'edit-profile' => 'Edit Profile',
+    'update-profile' => 'Update Profile',
+    'linked-accounts' => 'Linked Accounts',
+    'delete-account' => 'Delete Account',
+    'lans-attended' => 'LANs Attended',
+
+    // Roles
+    'roles' => 'Roles',
+    'role' => 'Role',
+    'role-assignment' => 'Role Assignment',
+    'role-assignments' => 'Role Assignments',
+    'assign-a-role' => 'Assign a Role',
+    'assign-role' => 'Assign Role',
+
+    // Dashboard
+    'dashboard' => 'Dashboard',
+
+    // Events
+    'event' => 'Event',
+    'events' => 'Events',
+    'list' => 'List',
+    'schedule' => 'Schedule',
+    'signups-open' => 'Signups Open',
+    'signups-close' => 'Signups Close',
+    'sign-up' => 'Sign up',
+
+    // Guides
+    'guide' => 'Guide',
+    'guides' => 'Guides',
+
+    // Logs
+    'logs' => 'Logs',
+    'log' => 'Log',
+    'data' => 'Data',
+    'time' => 'Time',
+    'level' => 'Level',
+    'message' => 'Message',
+    'context' => 'Context',
+    'html' => 'HTML',
+    'markdown' => 'Markdown',
+
+    // Games
+    'games' => 'Games',
+    'games-in-progress' => 'Games In Progress',
+    'in-progress' => 'In Progress',
+    'recently-played-games' => 'Recently Played Games',
+    'recent' => 'Recent',
+    'games-owned' => 'Games Owned',
+    'owned' => 'Owned',
+    'games-library' => 'Games Library',
+    'games-in-common' => 'Games In Common',
+    'games-history' => 'Games History',
+    'popular-games' => 'Popular Games',
+
+    // Navigation Links
+    'navigation-link' => 'Navigation Link',
+    'navigation-links' => 'Navigation Links',
+    'navigation' => 'Navigation',
+    'url' => 'URL',
+    'position' => 'Position',
+    'parent' => 'Parent',
+
+    // LANs
+    'lans' => 'LANs',
+    'lan' => 'LAN',
+    'dates' => 'Dates',
+    'times' => 'Times',
+    'duration' => 'Duration',
+    'x-hours' => ':x hours',
+    'attendees' => 'Attendees',
+    'attendee' => 'Attendee',
+
+    // Images
+    'images' => 'Images',
+    'image' => 'Image',
+    'upload-images' => 'Upload Images',
+    'upload' => 'Upload',
+    'filename' => 'Filename',
+    'files' => 'File(s)',
+    'copy-markdown' => 'Copy Markdown',
+
+    // Achievements
+    'achievements' => 'Achievements',
+    'achievement' => 'Achievement',
+    'award-an-achievement' => 'Award an Achievement',
+    'award' => 'Award',
+
+    // Venues
+    'venues' => 'Venues',
+    'venue' => 'Venue',
+    'street-address' => 'Street Address',
+    'map' => 'Map',
+
+    // Slides
+    'slides' => 'Slides',
+    'slide' => 'Slide',
+    'duration' => 'Duration',
+    'play' => 'Play',
+];
